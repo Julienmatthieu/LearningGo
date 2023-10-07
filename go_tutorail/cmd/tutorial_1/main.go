0deb758ffeb09b1f40833ccf99d5686f7f293de5
Julien Bogiraud <julien@bogiraud.com>
2023-10-07 15:12:40 +0200

    functions and param

--- go_tutorail/cmd/tutorial_1/main.go
+++ go_tutorail/cmd/tutorial_1/main.go
@@ -6,8 +6,20 @@ func main() {
 	text := "hello you"
 
 	printMe(text)
+
+	numerator := 11
+	denominator := 2
+	var result, remain int = intDivision(numerator, denominator)
+	fmt.Printf("The result of the division is %v and the remain is %v", result, remain)
 }
 
 func printMe(printValue string) {
 	fmt.Println(printValue)
+}
+
+func intDivision(numerator int, denominator int) (int, int) {
+	var result int = numerator / denominator
+	var remain int = numerator % denominator
+
+	return result, remain
 }
